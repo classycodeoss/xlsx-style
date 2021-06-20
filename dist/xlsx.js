ab672e35eb203070f47f92e261ed718d9febec7b
Jan-Filip Zagalak <zagi@classycode.com>
2021-06-20 12:39:01 +0200

    NOISSUE corrected JSZIP back to old api call of creating files together with folder with path expression, apparently it is still working

--- dist/xlsx.js
+++ dist/xlsx.js
@@ -11658,7 +11658,7 @@ function add_rels(rels, rId, f, type, relobj) {
 }
 
 function write_zip(wb, opts) {
-	/*
+	
 	if(wb && !wb.SSF) {
 		wb.SSF = SSF.get_table();
 	}
@@ -11679,77 +11679,66 @@ function write_zip(wb, opts) {
 	opts.cellXfs = [];
 	get_cell_style(opts.cellXfs, {}, {revssf:{"General":0}});
 
-	f = "core.xml";
-	var docPropsFolder = zip.folder("docProps");
-	docPropsFolder.file(f, write_core_props(wb.Props, opts));
+	f = "docProps/core.xml";
+	zip.file(f, write_core_props(wb.Props, opts));
 	ct.coreprops.push(f);
 	add_rels(opts.rels, 2, f, RELS.CORE_PROPS);
 
-	f = "app.xml";
+	f = "docProps/app.xml";
 	if(!wb.Props) wb.Props = {};
 	wb.Props.SheetNames = wb.SheetNames;
 	wb.Props.Worksheets = wb.SheetNames.length;
-	docPropsFolder.file(f, write_ext_props(wb.Props, opts));
+	zip.file(f, write_ext_props(wb.Props, opts));
 	ct.extprops.push(f);
 	add_rels(opts.rels, 3, f, RELS.EXT_PROPS);
 
 	if(wb.Custprops !== wb.Props && keys(wb.Custprops||{}).length > 0) {
-		f = "custom.xml";
-		docPropsFolder.file(f, write_cust_props(wb.Custprops, opts));
+		f = "docProps/custom.xml";
+		zip.file(f, write_cust_props(wb.Custprops, opts));
 		ct.custprops.push(f);
 		add_rels(opts.rels, 4, f, RELS.CUST_PROPS);
 	}
-	var xlFolder = zip.folder("xl");
-	f = "workbook." + wbext;
-	xlFolder.file(f, write_wb(wb, f, opts));
+
+	f = "xl/workbook." + wbext;
+	zip.file(f, write_wb(wb, f, opts));
 	ct.workbooks.push(f);
 	add_rels(opts.rels, 1, f, RELS.WB);
 
-	var worksheetsXlFolder = xlFolder.folder("worksheets");
 	for(rId=1;rId <= wb.SheetNames.length; ++rId) {
-		f = "sheet" + rId + "." + wbext;
-		worksheetsXlFolder.file(f, write_ws(rId-1, f, opts, wb));
+		f = "xl/worksheets/sheet" + rId + "." + wbext;
+		zip.file(f, write_ws(rId-1, f, opts, wb));
 		ct.sheets.push(f);
 		add_rels(opts.wbrels, rId, "worksheets/sheet" + rId + "." + wbext, RELS.WS);
 	}
 
 	if(opts.Strings != null && opts.Strings.length > 0) {
-		f = "sharedStrings." + wbext;
-		xlFolder.file(f, write_sst(opts.Strings, f, opts));
+		f = "xl/sharedStrings." + wbext;
+		zip.file(f, write_sst(opts.Strings, f, opts));
 		ct.strs.push(f);
 		add_rels(opts.wbrels, ++rId, "sharedStrings." + wbext, RELS.SST);
 	}
 
 	
-	var xlThemeFolder = xlFolder.folder("theme");
-	f = "theme1.xml";
-  	xlThemeFolder.file(f, write_theme(opts));
+	
+	f = "xl/theme/theme1.xml";
+    zip.file(f, write_theme(opts));
 	ct.themes.push(f);
 	add_rels(opts.wbrels, ++rId, "theme/theme1.xml", RELS.THEME);
 
 	
 
 	f = "xl/styles." + wbext;
-	xlFolder.file(f, write_sty(wb, f, opts));
+	zip.file(f, write_sty(wb, f, opts));
 	ct.styles.push(f);
 	add_rels(opts.wbrels, ++rId, "styles." + wbext, RELS.STY);
 
 	zip.file("[Content_Types].xml", write_ct(ct, opts));
-	var uscoreRelsFolder = zip.folder("_rels");
-	uscoreRelsFolder.file('.rels', write_rels(opts.rels));
-	var uscoreRelsXLFolder = xlFolder.folder("_rels");
-	uscoreRelsXLFolder.file('xl/_rels/workbook.' + wbext + '.rels', write_rels(opts.wbrels));
-*/
-
-var zip = new JSZip();
-zip.file("Hello.txt", "Hello World\n");
-var img = zip.folder("images");
-img.file("smile.gif", "Hello World\n");
-zip.generateAsync({type:"blob"})
-.then(function (blob) {
-    saveAs(blob, "hello.zip");
-});
-
+	zip.file('_rels/.rels', write_rels(opts.rels));
+	zip.file('xl/_rels/workbook.' + wbext + '.rels', write_rels(opts.wbrels));
+	zip.generateAsync({type:"blob"})
+		.then(function (blob) {
+			saveAs(blob, "hello.zip");
+		});
 	return zip;
 }
 function firstbyte(f,o) {
